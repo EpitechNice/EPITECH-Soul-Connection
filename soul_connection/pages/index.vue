b5dfa5c5eac71ad70358a8cb90e1847c525db3ab
Arthur <arthur.guyetand@epitech.eu>
2024-09-03 17:54:14 +0200

    add début home page card main stats

--- soul_connection/pages/index.vue
+++ soul_connection/pages/index.vue
@@ -3,5 +3,26 @@
         <h1>Soul Connection</h1>
         <h2>Dashboard</h2>
         <div class="separator"></div>
+        <div class="container">
+            <div class="row">
+                <div class="card" id="cardclient">
+                    <img src="../assets/User.svg"/>
+                    <p>2500</p>
+                    <p>Total Users</p>
+                </div>
+                <div class="card" id="cardcoach">
+                    <img src="../assets/User.svg"/>
+                    <p>2500</p>
+                    <p>Total coachs</p>
+                </div>
+                <div class="card" id="cardrdv">
+                    <img src="../assets/Briefcase.svg"/>
+                    <p>2500</p>
+                    <p>Total Rendez-vous</p>
+                </div>
+            </div>
+
+        </div>
     </div>
 </template>
+<style src="assets/css/homepage.css"></style>
