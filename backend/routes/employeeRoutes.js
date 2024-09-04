ac16242501545d7eb48ed017c26c621a035d38ae
Aeliondw <caroline.boilly@epitech.eu>
2024-09-04 17:01:09 +0200

    [+] Add all employee route in draft

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -1,10 +1,16 @@
 import express from "express"
-import { getEmployees } from "../controllers/employeeController.js"
+import { getEmployees, getEmployeeDetails } from "../controllers/employeeController.js"
 
 
 const router = express.Router();
 
 router.route("/employees").get(getEmployees);
+// router.route("/employees/login").post();
+// router.route("/employees/me").get();
+router.route("/employees/:id").get(getEmployeeDetails);
+// router.route("/employees/:id/image").get();
 
+// router.route("/employees/:id").put();
+// router.route("/employees/:id").delete();
 
 export default router;
