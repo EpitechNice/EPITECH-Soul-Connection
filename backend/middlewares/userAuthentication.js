99690de8622796bda129530408e0df7301fa456b
Aeliondw <caroline.boilly@epitech.eu>
2024-09-05 11:02:13 +0200

    [~] Small fix: authentification architecture

--- backend/middlewares/userAuthentication.js
+++ backend/middlewares/userAuthentication.js
@@ -4,25 +4,14 @@ import catchAsyncErrors from "./catchAsyncErrors.js";
 import User from "../models/userModel.js";
 
 //Check if user is auth
-export const isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
-    const {token} = req.cookies;
-    if (!token) {
+export const isAuth = catchAsyncErrors(async (req, res, next) => {
+    if (!req.cookies.token)
         return next(new ErrorHandler("Login required to access this resource", 401));
-    }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    var cookie = req.cookies.token;
+    const decoded = jwt.verify(cookie, process.env.SECRET_KEY);
+    if (!decoded)
+        return next(new ErrorHandler("Node weird refuse", 401));
 
     req.user = await User.findById(decoded.id);
-
     next();
 });
-
-export const authorizeRoles = (...types) => {
-    return (req, res, next) => {
-        if (!types.includes(req.user.role)) {
-            return next(new ErrorHandler(`Role '${req.user.type}' is not allowed to access this ressource`, 403));
-        }
-        next();
-    }
-
-};
