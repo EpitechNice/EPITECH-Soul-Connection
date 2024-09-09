9667bd69dba39728186b669b896de66121ec12e5
Arthur <arthur.guyetand@epitech.eu>
2024-09-09 17:10:41 +0200

    [+] add apitip in store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,14 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import { authApi } from "./api/authApi";
 import { employeeApi } from "./api/employeeApi.js";
+import { tipApi } from "./api/tipApi";
 
 export const store = configureStore({
   reducer: {
     // auth: userReducer,
     [employeeApi.reducerPath]: employeeApi.reducer,
+    [tipApi.reducerPath]: tipApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([
       employeeApi.middleware,
+      tipApi.middleware,
     ]),
 });
