7a87c6790c8a868336b41bbba5f33ac60d0d1ee1
Arthur <arthur.guyetand@epitech.eu>
2024-09-09 17:07:47 +0200

    [+] add tips page with db maybe

--- frontend/src/components/tips/Tips.jsx
+++ frontend/src/components/tips/Tips.jsx
@@ -1,30 +1,25 @@
 import React from "react";
+import { useGetTipsQuery } from "../../redux/api/tipApi";
 import SideMenu from "../layout/SideMenu";
 
 const TipsPage = () => {
-  const tips = [
-    "A super tip to help the coach with their customers",
-    "A great tip to enhance productivity",
-    "Another useful tip to boost client engagement",
-    "A tip to optimize communication with customers",
-    "A tip for better organization",
-    "Effective techniques for managing time",
-    "How to develop a stronger client relationship",
-    "How to keep customers satisfied",
-    "Tips for improving service quality"
-  ];
+  const { data: tips, error, isLoading } = useGetTipsQuery();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading tips</div>;
 
   return (
     <div className="tips-page pages">
-        <div className="col-12 col-lg-3">
-            <SideMenu />
-        </div>
+      <div className="col-12 col-lg-3">
+        <SideMenu />
+      </div>
       <h1>Tips</h1>
       <div className="separator"></div>
       <div className="tips-grid">
         {tips.map((tip, index) => (
           <div className="tip-card" key={index}>
-            <p>{tip}</p>
+            <p>{tip.title}</p>
+            <p>{tip.tip}</p>
           </div>
         ))}
       </div>
