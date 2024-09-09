53dc8f94c30d99d5daed4a6770b2b51d601a35a8
Aeliondw <caroline.boilly@epitech.eu>
2024-09-09 10:03:25 +0200

    [~] Revert commit <3020cde84ec081eb99cc7823274c1e09e4ea247f>

--- backend/middlewares/userAuthentication.js
+++ backend/middlewares/userAuthentication.js
@@ -7,11 +7,23 @@ import User from "../models/userModel.js";
 export const isAuth = catchAsyncErrors(async (req, res, next) => {
     if (!req.cookies.token)
         return next(new ErrorHandler("Login required to access this resource", 401));
+
     var cookie = req.cookies.token;
-    const decoded = jwt.verify(cookie, process.env.SECRET_KEY);
+    const decoded = jwt.verify(cookie, process.env.JWT_SECRET);
+
     if (!decoded)
         return next(new ErrorHandler("Node weird refuse", 401));
 
-    req.user = await User.findById(decoded.id);
+    req.user = await User.findByPk(decoded.id);
     next();
 });
+
+export const authorizeRoles = (...types) => {
+    return (req, res, next) => {
+        if (!types.includes(req.user.type)) {
+            return next(new ErrorHandler(`Role '${req.user.type}' is not allowed to access this ressource`, 403));
+        }
+        next();
+    }
+
+};
