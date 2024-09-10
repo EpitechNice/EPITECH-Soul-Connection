db33c103d1ef75f78c2c3183a7e1656e265ba7ee
Tech0ne <clement.piasco@epitech.eu>
2024-09-10 17:11:23 +0200

    [+] Added clothing "CRUD"

--- backend/controllers/clothingController.js
+++ backend/controllers/clothingController.js
@@ -4,6 +4,8 @@ import Clothing from "../models/clothingModel.js"
 import path from "path";
 import * as fs from "fs";
 
+const DEFAULT_IMAGE_PATH = "/usr/src/app/images/default.png"
+
 const mime = {
     html: 'text/html',
     txt: 'text/plain',
@@ -47,7 +49,7 @@ export const getClothes = catchAsyncErrors(async(req, res, next) => {
     }
 });
 
-//TODO: 422 Validation error
+// // TODO: 422 Validation error
 //DONE: Gives back the actual image data instead of it's path on the docker (not relevent)
 //Get an item of clothing image
 export const getClothingImg = catchAsyncErrors(async(req, res, next) => {
@@ -68,4 +70,76 @@ export const getClothingImg = catchAsyncErrors(async(req, res, next) => {
     s.on('error', function () {
         res.status(404).end('Not found');
     });
+});
+
+export const createClothing = catchAsyncErrors(async (req, res) => {
+    try {
+        const modifiedBody = {
+            ...req.body,
+            image_path: DEFAULT_IMAGE_PATH,
+        };
+
+        const clothing = await Clothing.create(modifiedBody);
+
+        res.status(201).json({
+            message: "New clothing created successfully",
+            clothing,
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.status(400).json({
+            error: "Failed to create clothing",
+            details: err.message
+        });
+    }
+});
+
+export const updateClothing = catchAsyncErrors(async (req, res, next) => {
+    const id = req.params.id;
+
+    if (!id ) {
+        return next(new ErrorHandler("Invalid clothing ID", 400));
+    }
+
+    try {
+        const fieldUpdated = await Clothing.update(
+            { ...req.body },
+            { where: { id: id } }
+        );
+
+        res.status(200).json({
+            "fieldUpdated": fieldUpdated,
+        });
+    } catch (err) {
+        console.error(err);
+        next(new ErrorHandler("An error occurred while updating the clothing", 500));
+    }
+});
+
+export const deleteClothing = catchAsyncErrors(async (req, res, next) => {
+    const id = req.params.id
+
+    if (!id || isNaN(id)) {
+        return next(new ErrorHandler("Invalid clothing ID", 400));
+    }
+
+    try {
+        const result = await Clothing.destroy({
+            where: { id: id },
+            returning: true
+        });
+
+        if (result[0] === 0) {
+            return next(new ErrorHandler("Clothing not found", 404));
+        }
+
+        res.status(200).json({
+            message: "Clothing deleted successfully",
+            count: result[0]
+        });
+    } catch (err) {
+        console.error(err);
+        next(new ErrorHandler("An error occurred while deleting the clothing", 500));
+    }
 });
