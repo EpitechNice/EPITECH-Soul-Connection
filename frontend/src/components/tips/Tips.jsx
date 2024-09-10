5135343d9301ac84665793c713699bfa7b474603
MickaelFrigo <mickael.frigo@epitech.eu>
2024-09-10 11:23:00 +0200

    [+] Tips Page completed :)

--- frontend/src/components/tips/Tips.jsx
+++ frontend/src/components/tips/Tips.jsx
@@ -15,21 +15,25 @@ const Tips = () => {
 
     if (isLoading) return <Loader />;
 
+    const tipsArray = Array.isArray(data) ? data : data?.tips;
+
     return (
         <div className="tips-page pages">
             <div className="col-12 col-lg-3">
                 <SideMenu />
             </div>
-          <h1>Tips</h1>
-          <div className="separator"></div>
-          <div className="tips-grid">
-            {/* ici */}
-              <div className="tip-card" key={index}>
-                <p>{tip}</p>
-              </div>
-            ))}
-          </div>
+            <h1>Tips</h1>
+            <div className="separator"></div>
+            <div className="tips-grid">
+                {tipsArray?.map((tip, index) => (
+                    <div className="tip-card" key={index}>
+                        <h3>{tip.title}</h3>
+                        <p>{tip.tip}</p>
+                    </div>
+                ))}
+            </div>
         </div>
-      );
-    };
+    );
+};
+
 export default Tips;
