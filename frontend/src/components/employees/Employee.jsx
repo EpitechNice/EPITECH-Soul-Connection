be04db3a7e0d9b9c7fed3467bb70d6c120cdd09d
MickaelFrigo <mickael.frigo@epitech.eu>
2024-09-11 15:25:26 +0200

    [+] Upgrade Employee table

--- frontend/src/components/employees/Employee.jsx
+++ frontend/src/components/employees/Employee.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import SideMenu from "../layout/SideMenu";
-import { useGetEmployeesQuery } from '../../redux/api/employeeApi';
+import { useGetEmployeesQuery, useGetEmployeeImgQuery } from '../../redux/api/employeeApi';
 import toast from "react-hot-toast";
 import Loader from '../layout/Loader';
 import IconDownload from '../../assets/Download.svg';
+import SortIcon from '../../assets/Sort.svg';
+import SettingsIcon from '../../assets/Settings.svg';
 
 const Employees = () => {
     const { data, isLoading, error, isError } = useGetEmployeesQuery();
     const [users, setUsers] = useState([]);
-    const [showModal, setShowModal] = useState(false);
-    const [selectedUser, setSelectedUser] = useState(null);
-    const [userClients, setUserClients] = useState({});
+    const [searchTerm, setSearchTerm] = useState('');
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [sortOrder, setSortOrder] = useState('default');
+    const { data: employeeImgData } = "../../assets/images/employees";
 
     useEffect(() => {
         if (data) {
@@ -21,31 +24,18 @@ const Employees = () => {
         }
     }, [data, isError, error, isLoading]);
 
-    const handleOpenModal = (user) => {
-        setSelectedUser(user);
-        setShowModal(true);
-    };
+    useEffect(() => {
+        handleSortChange(sortOrder);
+    }, [sortOrder]);
 
-    const handleCloseModal = () => {
-        setShowModal(false);
-    };
-
-    const handleBulkAction = () => {
-        const selectedClients = userClients[selectedUser.id] || [];
-        console.log("Selected clients: ", selectedClients);
-    };
-
-    const handleClientSelection = (clientId) => {
-        setUserClients(prevState => {
-            const userId = selectedUser.id;
-            const currentUserClients = prevState[userId] || [];
-
-            const updatedClients = currentUserClients.includes(clientId)
-                ? currentUserClients.filter(id => id !== clientId)
-                : [...currentUserClients, clientId];
-
-            return { ...prevState, [userId]: updatedClients };
-        });
+    const handleSortChange = (order) => {
+        let sortedUsers = [...users];
+        if (order === 'asc') {
+            sortedUsers.sort((a, b) => a.name.localeCompare(b.name));
+        } else if (order === 'desc') {
+            sortedUsers.sort((a, b) => b.name.localeCompare(a.name));
+        }
+        setUsers(sortedUsers);
     };
 
     const downloadCSV = () => {
@@ -69,10 +59,23 @@ const Employees = () => {
         a.click();
         window.URL.revokeObjectURL(url);
     };
-    
+
+    const handleBulkAction = () => {
+        const selectedAction = document.querySelector('.bulk-action-select').value;
+        if (selectedAction === 'delete') {
+            // Delete selected users
+        } else if (selectedAction === 'email') {
+            // Send email to selected users
+        }
+    };
+
+    const filteredUsers = users.filter(user => {
+        const fullName = `${user.name} ${user.surname}`.toLowerCase();
+        return fullName.includes(searchTerm.toLowerCase());
+    });
 
     if (isLoading) return <Loader />;
-    const EmployeeArray = Array.isArray(users) ? users : [];
+    const EmployeeArray = Array.isArray(filteredUsers) ? filteredUsers : [];
 
     return (
         <div className="employees-page">
@@ -86,89 +89,101 @@ const Employees = () => {
                         <p className="page-subtitle">You have total {users.length} coaches.</p>
                     </div>
                     <div className='group-button'>
-                    <button className="export-button" onClick={downloadCSV}>
-                        <img src={IconDownload} alt="Download Icon" />
-                        Export
-                    </button>
-                    <button className="add-button">+</button>
+                        <button className="export-button" onClick={downloadCSV}>
+                            <img src={IconDownload} alt="Download Icon" />
+                            Export
+                        </button>
+                        <button className="add-button">+</button>
                     </div>
                 </div>
                 <div className="all-content-table">
-                <div className="actions">
-                <div className="user-table-header-cell bulk-action-dropdown button-left-group">
-                    <select className="bulk-action-select">
-                        <option value="">Bulk Action</option>
-                        <option value="delete">Delete</option>
-                        <option value="email">Send Email</option>
-                    </select>
-                    <button className="bulk-action-button" onClick={() => handleBulkAction()}>
-                        Apply
-                    </button>
-                </div>
-                </div>
-                <div className="user-table">
-                    <div className="user-table-header">
-                        <div className="user-table-header-cell">Coach</div>
-                        <div className="user-table-header-cell">Email</div>
-                        <div className="user-table-header-cell">Phone</div>
-                        <div className="user-table-header-cell">Number of customers</div>
-                        <div id="action-title" className="user-table-header-cell">Actions</div>
-                    </div>
-                    <div className="user-table-body">
-                        {EmployeeArray.length === 0 ? (
-                            <div className="user-table-row">
-                                <div className="user-table-cell">No data available</div>
-                            </div>
-                        ) : (
-                            EmployeeArray.map((user, index) => (
-                                <div className="user-table-row" key={user.id}>
-                                    <div className="user-table-cell">
-                                        <div className="avatar-circle">
-                                            {user.name.charAt(0)}{user.surname?.charAt(0)}
-                                        </div>
-                                        <div className="user-info">
-                                            <p className="user-name">{user.name} {user.surname}</p>
-                                        </div>
-                                    </div>
-                                    <div className="user-table-cell">{user.email}</div>
-                                    <div className="user-table-cell">{user.phone || "N/A"}</div>
-                                    <div className="user-table-cell">{user.customers?.length || "N/A"}</div>
-                                    <div id="action-button" className="user-table-cell">
-                                        <button className="edit-button" onClick={() => handleOpenModal(user)}>
-                                            ...
-                                        </button>
-                                    </div>
+                    <div className="actions">
+                        <div className="user-table-header-cell bulk-action-dropdown button-left-group">
+                            <select className="bulk-action-select">
+                                <option value="">Bulk Action</option>
+                                <option value="delete">Delete</option>
+                                <option value="email">Send Email</option>
+                            </select>
+                            <button className="bulk-action-button" onClick={handleBulkAction}>
+                                Apply
+                            </button>
+                        <div className="group-button-table">
+                            <div className="search-bar">
+                                <div className="search-wrapper">
+                                    <input
+                                        type="text"
+                                        placeholder="Search coaches..."
+                                        value={searchTerm}
+                                        onChange={(e) => setSearchTerm(e.target.value)}
+                                        className="search-input"
+                                    />
+                                    <i className="search-icon fas fa-search"></i>
                                 </div>
-                            ))
-                        )}
+                            </div>
+                            <div className="sort-dropdown">
+                                <img
+                                    src={SortIcon}
+                                    alt="Sort Icon"
+                                    className="sort-icon"
+                                    onClick={() => setDropdownOpen(!dropdownOpen)}
+                                />
+                                {dropdownOpen && (
+                                    <ul className="sort-options">
+                                        <li onClick={() => handleSortChange('default')}>Default</li>
+                                        <li onClick={() => handleSortChange('asc')}>A - Z</li>
+                                        <li onClick={() => handleSortChange('desc')}>Z - A</li>
+                                    </ul>
+                                )}
+                            </div>
+                            <div className="sort-dropdown">
+                                <img src={SettingsIcon} alt="Settings Icon" className="sort-icon" />
+                            </div>
+                        </div>
+                        </div>
                     </div>
-                </div>
+                    <div className="user-table">
+                        <div className="user-table-header">
+                            <div className="user-table-header-cell">Coach</div>
+                            <div className="user-table-header-cell">Email</div>
+                            <div className="user-table-header-cell">Phone</div>
+                            <div className="user-table-header-cell">Number of customers</div>
+                            <div id="action-title" className="user-table-header-cell">Actions</div>
+                        </div>
+                        <div className="user-table-body">
+                            {EmployeeArray.length === 0 ? (
+                                <div className="user-table-row">
+                                    <div className="user-table-cell">No data available</div>
+                                </div>
+                            ) : (
+                                EmployeeArray.map((user) => (
+                                    <div className="user-table-row" key={user.id}>
+                                        <div className="user-table-cell">
+                                            {employeeImgData?.[user.id] ? (
+                                                <img src={employeeImgData[user.id]} alt="User" className="user-image" />
+                                            ) : (
+                                                <div className="avatar-circle">
+                                                    {user.name.charAt(0)}{user.surname?.charAt(0)}
+                                                </div>
+                                            )}
+                                            <div className="user-info">
+                                                <p className="user-name">{user.name} {user.surname}</p>
+                                            </div>
+                                        </div>
+                                        <div className="user-table-cell">{user.email}</div>
+                                        <div className="user-table-cell">{user.phone || "N/A"}</div>
+                                        <div className="user-table-cell">{user.customers?.length || "N/A"}</div>
+                                        <div id="action-button" className="user-table-cell">
+                                            <button className="edit-button">
+                                                ...
+                                            </button>
+                                        </div>
+                                    </div>
+                                ))
+                            )}
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            {showModal && selectedUser && (
-                <div className="modal">
-                    <div className="modal-content">
-                        <h3>Select Clients for {selectedUser?.name} {selectedUser?.surname}</h3>
-                        <div className="client-list">
-                            {users.map(user => (
-                                <div key={user.id}>
-                                    <input
-                                        type="checkbox"
-                                        id={`client-${user.id}`}
-                                        checked={(userClients[selectedUser.id] || []).includes(user.id)}
-                                        onChange={() => handleClientSelection(user.id)}
-                                    />
-                                    <label htmlFor={`client-${user.id}`}>{user.name} {user.surname}</label>
-                                </div>
-                            ))}
-                        </div>
-                        <button className="save-button" onClick={handleCloseModal}>
-                            Save
-                        </button>
-                    </div>
-                </div>
-            )}
         </div>
     );
 };
